abf64687ce59be940c58e6f4370ce03f087ba3a3
murshid <muchimurshid.790@gmail.com>
2021-07-02 16:08:34 +0430

    database changed

--- noteapp/__init__.py
+++ noteapp/__init__.py
@@ -5,7 +5,7 @@ from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://hvrgcxixtmjajb:c548436cdd0df0b4048ffa67fc04beda5d49e18b73bb0bf58e35f5b8a1f48247@ec2-50-17-255-120.compute-1.amazonaws.com:5432/de9rml1je07q27?sslmode=require'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
